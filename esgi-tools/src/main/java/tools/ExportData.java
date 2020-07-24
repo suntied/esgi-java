f488ee9a1544a90889651f360b69b60d4f082580
YlanaLefebvre <58032937+YlanaLefebvre@users.noreply.github.com>
2020-07-24 23:53:13 +0200

    Update ExportData exportToPDF

--- esgi-tools/src/main/java/tools/ExportData.java
+++ esgi-tools/src/main/java/tools/ExportData.java
@@ -1,13 +1,14 @@
 package tools;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Path;
+import javax.swing.text.Document;
+import java.io.*;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
 public class ExportData {
 
@@ -15,7 +16,7 @@ public class ExportData {
         //Todo export String to csv file
         List<String> tabString = new Arrays.asList(currenyData.split(","));
 
-        BufferedWriter writeToCSV = File.newBufferedWriter(Paths.get("users-with-header.csv"));
+        BufferedWriter writeToCSV = File.newBufferedWriter(Paths.get("RapportCSV.csv"));
         writeToCSV.write("Date, value");
         writeToCSV.newLine();
 
@@ -28,12 +29,15 @@ public class ExportData {
 
     }
 
-    public static void exportToPdf(String currenyData) {
+    public static void exportToPdf(String currenyData) throws FileNotFoundException {
         //Todo export String to Pdf file
         List<String> tabString;
         tabString = new Arrays.asList(currenyData.split(","));
-
-
-
+        String out = "ReportingSupervision.pdf";
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document,
+                new FileOutputStream("./rapport.pdf"));
+        document.add(new Paragraph("Rapport :"));
+        document.add(new Paragraph(tabString));
     }
 }
