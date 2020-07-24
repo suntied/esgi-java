bbbca257691e2bbe21258a70a216e8e4fe74e410
YlanaLefebvre <58032937+YlanaLefebvre@users.noreply.github.com>
2020-07-24 23:03:39 +0200

    Update ExportData
    Reste une erreur je n'arrive pas à la comprendre : Error:java: exporting a package from system module jdk.scripting.nashorn is not allowed with --release

--- esgi-tools/src/main/java/tools/ExportData.java
+++ esgi-tools/src/main/java/tools/ExportData.java
@@ -1,10 +1,39 @@
 package tools;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 public class ExportData {
-    public static void exportToCsv(String currenyData){
+
+    public static void exportToCsv(String currenyData) throws IOException {
         //Todo export String to csv file
+        List<String> tabString = new Arrays.asList(currenyData.split(","));
+
+        BufferedWriter writeToCSV = File.newBufferedWriter(Paths.get("users-with-header.csv"));
+        writeToCSV.write("Date, value");
+        writeToCSV.newLine();
+
+        for (String stringList : tabString){
+            writeToCSV.append(String.join(",", stringList));
+            writeToCSV.newLine();
+
+        }
+
+
     }
-    public static void exportToPdf(String currenyData){
+
+    public static void exportToPdf(String currenyData) {
         //Todo export String to Pdf file
+        List<String> tabString;
+        tabString = new Arrays.asList(currenyData.split(","));
+
+
+
     }
 }
